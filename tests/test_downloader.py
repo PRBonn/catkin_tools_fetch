28af262c4bc994e2b5081d77f76a08a579c12244
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2016-11-15 13:37:01 +0100

    remove repo from tests because of access violation

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -15,9 +15,7 @@ class TestDownloader(unittest.TestCase):
 
     def test_repository_exists(self):
         http_url = "https://github.com/niosus/catkin_tools_fetch"
-        git_url = "git@github.com:niosus/catkin_tools_fetch.git"
         self.assertTrue(Downloader.repository_exists(http_url))
-        self.assertTrue(Downloader.repository_exists(git_url))
 
         wrong_url = "https://github.com/niosus"
         self.assertFalse(Downloader.repository_exists(wrong_url))
